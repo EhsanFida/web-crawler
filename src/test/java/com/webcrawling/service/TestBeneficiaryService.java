44836ca7c50a06cbc8cb57bd48175466f1e3d1c4
EhsanFida <ehsanfida615@gmail.com>
2023-10-21 16:35:02 +0500

    Junit Integration. Some test cases for beneficiary service

---
+++ src/test/java/com/webcrawling/service/TestBeneficiaryService.java
@@ -0,0 +1,51 @@
+package com.webcrawling.service;
+
+import com.webcrawling.entities.Beneficiary;
+import com.webcrawling.repositories.BeneficiaryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@SpringBootTest
+public class TestBeneficiaryService {
+    @Autowired
+    BeneficiaryService beneficiaryService;
+
+    @Autowired
+    BeneficiaryRepository beneficiaryRepository;
+
+    @BeforeEach
+    public void setup() {
+        beneficiaryRepository.deleteAll();
+
+        Beneficiary bene = new Beneficiary();
+        bene.setFirstName("Ehsan");
+        bene.setMiddleName("");
+        bene.setLastName("Fida");
+        bene.setContactPhoneType("mobile");
+        bene.setContactPhoneNumber("03455165351");
+        bene.setIsActive(true);
+        beneficiaryService.createBeneficiary(bene);
+    }
+
+    @Test
+    public void testGetAllBeneficiaries() {
+        assertEquals(1, beneficiaryService.getBeneficiaries().size());
+    }
+
+    @Test
+    public void testGetBeneficiaryById() {
+        Beneficiary beneficiary = new Beneficiary();
+        beneficiary.setFirstName("Babar");
+        beneficiary.setLastName("Taimour");
+        beneficiary.setIsActive(true);
+        Beneficiary beneficiary1 = beneficiaryService.createBeneficiary(beneficiary);
+
+        assertNotNull(beneficiaryService.getBeneficiaryById(beneficiary1.getId()));
+    }
+}
