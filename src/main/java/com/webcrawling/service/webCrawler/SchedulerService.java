3ac6e9a55f69564d2757fff45b22ef50b5fcbf2f
Assad ul Hassan <assad.ulhassan@systemsltd.com>
2023-11-29 14:52:33 +0500

    Test executor with callable implementation

--- src/main/java/com/webcrawling/service/webCrawler/SchedulerService.java
+++ src/main/java/com/webcrawling/service/webCrawler/SchedulerService.java
@@ -1,57 +1,47 @@
 package com.webcrawling.service.webCrawler;
 
 import com.webcrawling.service.webCrawler.impl.WebCrawlerServiceImpl;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import org.slf4j.Logger;
 
-
-@Configuration
 @Service
-@Component
-
+@Slf4j
 public class SchedulerService {
-    private static final Logger logger= LoggerFactory.getLogger(SchedulerService.class);
     private final ExecutorService executorService = Executors.newFixedThreadPool(10);
     @Autowired
     WebCrawlerServiceImpl webCrawlerService;
     @Value("${urls}")
-    //List<String> urls = new ArrayList<>();
-    List<Callable<String>> urls = new ArrayList<>();
+    private String url;
+    private List<String> urls;
+
+    @PostConstruct
+    public void init() {
+        urls = new ArrayList<>();
+        urls.addAll(Arrays.stream(url.split(",")).toList());
+    }
+
     // Run after every Five(5) Minutes
     @Scheduled(fixedRate = 500000)
-        public void scheduledCrawl() {
-        urls.add(() -> webCrawlerService.crawlWebsite(urls, 10, 10));
-        /*executorService.execute(new Runnable() {
-            public void run() {
-                logger.info("Asynchronous task");
-                for (String url : urls){
-                    logger.info("in loop"+url);
-                    webCrawlerService.crawlWebsite(url, 10, 10);
-                }
-            }
-        });*/
-        executorService.invokeAll();
+    public void scheduledCrawl() throws InterruptedException {
+        List<Callable<String>> callables = new ArrayList<>();
+        urls.forEach(val -> {
+            callables.add(new ServiceProcessors(val, webCrawlerService));
+            //executorService.submit(new ServiceProcessors(val, webCrawlerService));
+        });
+        executorService.invokeAll(callables);
         executorService.shutdown();
-    }
-    public Callable newCallable(){
-        return newCallable() {
-            @Override
-                    public Object call() throws Exception {
-            }
-        }
-        }
     }
 
 }
